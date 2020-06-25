d8be183d38f7c222bf079ec7e09fdcd19a9ed738
chmoder <tom.bz2@gmail.com>
2020-06-24 21:22:30 -0500

    deadpool redis implementation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use actix_web::{web, App, Responder, HttpServer};
+use deadpool_redis::{cmd,Config, Pool};
 
-async fn index(info: web::Path<(String, u32)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.0, info.1)
+async fn index(info: web::Path<(String, u32)>, redis_pool: web::Data<Pool>) -> impl Responder {
+    let mut conn = redis_pool.get().await.unwrap();
+
+    let item: String = cmd("GET")
+        .arg(&["tcross"])
+        .query_async(&mut conn)
+        .await.unwrap_or_default();
+
+    format!("Hello {}! id:{} hash:{}", info.0, info.1, item)
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(
+    let cfg = Config::from_env("prefix").unwrap();
+    let pool = cfg.create_pool().unwrap();
+
+    HttpServer::new(move || App::new().data(pool.clone()).service(
         web::resource("/{name}/{id}/index.html").to(index))
     )
-        .bind("127.0.0.1:8080")?
+        .workers(num_cpus::get())
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
