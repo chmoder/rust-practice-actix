86dd5ed2ee7a56dd9a47a701a1a3264c64e57b2b
chmoder <tom.bz2@gmail.com>
2020-06-24 23:07:00 -0500

    make key dynamic and add a value if one does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use actix_web::{web, App, Responder, HttpServer};
-use deadpool_redis::{cmd,Config, Pool};
+use deadpool_redis::{cmd, Config, Pool};
 
-async fn index(info: web::Path<(String, u32)>, redis_pool: web::Data<Pool>) -> impl Responder {
+async fn get_from_redis(key: String, redis_pool: web::Data<Pool>) -> String {
     let mut conn = redis_pool.get().await.unwrap();
 
-    let item: String = cmd("GET")
-        .arg(&["tcross"])
+    return cmd("GET")
+        .arg(&[key])
         .query_async(&mut conn)
         .await.unwrap_or_default();
+}
 
-    format!("Hello {}! id:{} hash:{}", info.0, info.1, item)
+async fn add_to_redis(key: String, val: String, redis_pool: web::Data<Pool>) {
+    let mut conn = redis_pool.get().await.unwrap();
+
+    return cmd("SET")
+        .arg(&[key, val])
+        .execute_async(&mut conn)
+        .await.unwrap_or_default();
+}
+
+async fn index(path_params: web::Path<(String, u32)>, redis_pool: web::Data<Pool>) -> impl Responder {
+    let mut item= get_from_redis(path_params.0.clone(), redis_pool.clone()).await;
+
+    if item.is_empty() {
+        add_to_redis(path_params.0.clone(), "42".to_string(), redis_pool.clone()).await;
+        item = get_from_redis(path_params.0.clone(), redis_pool.clone()).await;
+    }
+
+    format!("Hello {}! id:{} redis_value:{}", path_params.0, path_params.1, item)
 }
 
 #[actix_rt::main]
